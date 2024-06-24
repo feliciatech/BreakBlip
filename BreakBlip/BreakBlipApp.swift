7c4610ce5de21e91e584b45498690b59dc7d7580
feliciatech <150612702+feliciatech@users.noreply.github.com>
2024-06-24 22:58:59 +0200

    Initial Commit

---
+++ BreakBlip/BreakBlipApp.swift
@@ -0,0 +1,32 @@
+//
+//  BreakBlipApp.swift
+//  BreakBlip
+//
+//  Created by EW on 24/06/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BreakBlipApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
